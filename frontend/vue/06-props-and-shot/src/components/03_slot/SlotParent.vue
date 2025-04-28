6ad9ebca2fb6b22ed53e8ed7fa7f0e2f4ba87b99
Lee-gi-yeun <leessjjgg123456@naver.com>
2025-04-28 11:04:41 +0900

    vue

---
+++ frontend/vue/06-props-and-shot/src/components/03_slot/SlotParent.vue
@@ -0,0 +1,23 @@
+<script setup>
+import SlotChild from "@/components/03_slot/SlotChild.vue";
+import {ref} from "vue";
+const slotData = ref('hello slot');
+</script>
+
+<template>
+  <div class="container">
+    <h1>Parent</h1>
+    slot data 변경 : <input type="text" v-model="slotData">
+    <SlotChild>
+      <h3>{{slotData}}</h3>
+    </SlotChild>
+    <!--
+    Slot의 name 속성 활용하기
+    -->
+
+  </div>
+</template>
+
+<style scoped>
+
+</style>
